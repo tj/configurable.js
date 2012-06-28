271fa2944ccccf1910789e2ca5d16d711e8e1461
TJ Holowaychuk <tj@vision-media.ca>
2012-06-27 18:19:21 -0700

    add .enabled() / .disabled()

--- test/configurable.js
+++ test/configurable.js
@@ -1,46 +1,66 @@
 
 var Configurable = require('..');
 
-var obj = {};
+describe('obj.', function(){
+  var obj;
 
-Configurable(obj);
-
-describe('.set(name, val)', function(){
-  it('should set .settings[name] to val', function(){
-    obj.set('foo', 'bar');
-    obj.settings.foo.should.equal('bar');
+  beforeEach(function(){
+    obj = Configurable({});
   })
 
-  it('should return itself', function(){
-    obj.set('foo', 'bar').should.equal(obj);
-  })
-})
+  describe('.set(name, val)', function(){
+    it('should set .settings[name] to val', function(){
+      obj.set('foo', 'bar');
+      obj.settings.foo.should.equal('bar');
+    })
 
-describe('.get(name)', function(){
-  it('should get .settings[name]', function(){
-    obj.set('foo', 'bar');
-    obj.get('foo').should.equal('bar');
-  })
-})
-
-describe('.enable(name)', function(){
-  it('should set .settings[name] to true', function(){
-    obj.enable('proxy');
-    obj.get('proxy').should.equal(true);
+    it('should return itself', function(){
+      obj.set('foo', 'bar').should.equal(obj);
+    })
   })
 
-  it('should return itself', function(){
-    obj.enable('proxy').should.equal(obj);
-  })
-})
-
-describe('.disable(name)', function(){
-  it('should set .settings[name] to false', function(){
-    obj.disable('proxy');
-    obj.get('proxy').should.equal(false);
+  describe('.get(name)', function(){
+    it('should get .settings[name]', function(){
+      obj.set('foo', 'bar');
+      obj.get('foo').should.equal('bar');
+    })
   })
 
-  it('should return itself', function(){
-    obj.disable('proxy').should.equal(obj);
+  describe('.enable(name)', function(){
+    it('should set .settings[name] to true', function(){
+      obj.enable('proxy');
+      obj.get('proxy').should.equal(true);
+    })
+
+    it('should return itself', function(){
+      obj.enable('proxy').should.equal(obj);
+    })
+  })
+
+  describe('.disable(name)', function(){
+    it('should set .settings[name] to false', function(){
+      obj.disable('proxy');
+      obj.get('proxy').should.equal(false);
+    })
+
+    it('should return itself', function(){
+      obj.disable('proxy').should.equal(obj);
+    })
+  })
+
+  describe('.enabled(name)', function(){
+    it('should return true when enabled', function(){
+      obj.enabled('proxy').should.be.false;
+      obj.enable('proxy');
+      obj.enabled('proxy').should.be.true;
+    })
+  })
+
+  describe('.disabled(name)', function(){
+    it('should return true when disabled', function(){
+      obj.disabled('proxy').should.be.true;
+      obj.enable('proxy');
+      obj.disabled('proxy').should.be.false;
+    })
   })
 })
